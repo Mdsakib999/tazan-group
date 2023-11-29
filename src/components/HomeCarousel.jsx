12638fe89fabaaf445fe87fe3dbd753b4a23b64a
Mdsakib999 <sakibabrar999@gmail.com>
2023-11-29 15:43:18 +0600

    dynamic countup added

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CountUp from "react-countup";
 
 const HomeCarousel = () => {
   return (
@@ -9,7 +10,7 @@ const HomeCarousel = () => {
       <div className="w-[80%] mx-auto mt-3 mb-6">
       <div className="carousel rounded-md">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src="https://i.ibb.co/G2xmZpT/Tg2.jpg" className="w-full" />
+          <img src="https://www.kalmbachfeeds.com/wp-content/uploads/2022/05/Kalmbach-Article-Blog-Header-5.3.22-1024x536.jpg  " className="w-full h-[200px] lg:h-[400px]" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -21,7 +22,8 @@ const HomeCarousel = () => {
         </div>
 
         <div id="slide2" className="carousel-item relative w-full">
-          <img src="https://i.ibb.co/P4XSVkq/Tg1.jpg" className="w-full" />
+          <img src="https://proagri.co.za/wp-content/uploads/2021/11/layer-farming.jpg" className="w-full h-[200px] lg:h-[400px]" />
+          {/* <p>   https://i.ibb.co/P4XSVkq/Tg1.jpg</p> */}
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -59,24 +61,27 @@ const HomeCarousel = () => {
       </div>
 
 
-
       {/* Counting box */}
       <div className="bg-slate-100 py-20 flex justify-around flex-col lg:flex-row gap-y-10 lg:gap-y-0">
       <div className="w-[80%] mx-auto lg:mx-0  lg:w-[18%] text-center text-white text-3xl font-bold rounded-lg shadow-lg py-6 bg-gradient-to-r from-green-600 to-lime-400 hover:from-lime-300 hover:to-green-600">
-      <h1 className="text-6xl mb-2">5000+</h1>
-      <p className="lg:text-2xl text-lg">Customer's</p>
+      <h1 className="text-6xl mb-2">
+        <CountUp enableScrollSpy={true} end={5000}></CountUp>+
+      </h1>
+      <p className="lg:text-xl text-lg">Happy Customers</p>
       </div>
       <div className="w-[80%] mx-auto lg:mx-0  lg:w-[18%] text-center text-white text-3xl font-bold rounded-lg shadow-lg py-6 bg-gradient-to-r from-green-600 to-lime-400 hover:from-lime-300 hover:to-green-600">
-      <h1 className="text-6xl mb-2">50+</h1>
+      <h1 className="text-6xl mb-2"><CountUp enableScrollSpy={true} end={50}></CountUp>+</h1>
       <p className="text-lg lg:text-2xl">Medicine Product</p>
       </div>
 
        <div className="w-[80%] mx-auto lg:mx-0  lg:w-[18%] text-center text-white text-3xl font-bold rounded-lg shadow-lg py-6 bg-gradient-to-r from-green-600 to-lime-400 hover:from-lime-300 hover:to-green-600">
-      <h1 className="text-6xl mb-2">30+</h1>
+      <h1 className="text-6xl mb-2"><CountUp enableScrollSpy={true} end={30}></CountUp>+</h1>
       <p className="text-lg lg:text-xl">Imported Countries</p>
     </div>
 
       </div>
+
+
     </div>
   );
 };
