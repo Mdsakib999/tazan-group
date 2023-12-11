9bc09ec9da403e67145a5cf6fb61084f1f9fbfc2
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-11 14:24:48 +0600

    single product page added

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
-import productData from '../../public/data.json';
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import productData from "../../public/data.json";
 
 const ProductDetails = () => {
-    const {id} = useParams();
-    const products = productData.find((p) => p.id === parseInt(id, 10));
+  const { id } = useParams();
+  const products = productData.find((p) => p.id === parseInt(id, 10));
 
-    console.log(id);
-    console.log(products);
+  console.log(id);
+  console.log(products);
 
-    return (
-        <div className='w-[70%] mx-auto'>
-            <h1 className='text-center font-bold text-4xl'>Product Details</h1>
-            
-            <div className='my-6 flex items-center gap-6'>
-                <div>
-                    <img className='w-[300px] rounded-lg' src={products.image} alt="" />
-                </div>
+  return (
+    <div className="b w-[75%] mx-auto">
+      <h1 className="text-center font-bold text-4xl">Product Details</h1>
 
-                <div className='text-xl'>
-                    <p>Name: {products.productName}</p>
-                    <p>Composition: {products.composition}</p>
-                    <p>Dosage: {products.dosage}</p>
-                    <p></p>
-                    <p>Packing: {products.packing} bag</p>
-                    <p>Shelf Life: {products.shelfLife} </p>
-                    <p>Type: {products.type}</p>
-                    <p>Company: {products.company}</p>
-                    <p>Made In {products.country}</p>
-                    <p>Price: {products.price}</p>
-                    <p>Rating: {products.rating}</p>
-                </div>
-            </div>
+      <div className="my-10 lg:flex items-center gap-10">
+        <div>
+          <img className="w-[300px] rounded-lg" src={products.image} alt="" />
         </div>
-    );
+
+        <div className="text-xl">
+          <p className="font-semibold text-2xl mt-3 mb-2">
+            Name: {products.productName}
+          </p>
+          <p className="mb-2">Composition: {products.composition}</p>
+          <p className="mb-2">Dosage: {products.dosage}</p>
+
+          <p className="mb-2">Packing: {products.packing} bag</p>
+          <p className="mb-2">Shelf Life: {products.shelfLife} </p>
+          <p className="mb-2">Type: {products.type}</p>
+          <p className="mb-2">Company: {products.company}</p>
+          <p className="mb-2">Made In {products.country}</p>
+          <div>
+            <p className="mb-2"> {products.price}</p>
+            <p className="mb-2">Rating: {products.rating}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
