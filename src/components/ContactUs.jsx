76e1426c288fd17d33fca740c0dceffc33d69ef1
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-19 12:38:49 +0600

    animation added at contact page

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import animation from "../assets/Animation - 1702966470394.json";
+import Lottie from "lottie-react";
 
 const ContactUs = () => {
   return (
@@ -6,23 +8,17 @@ const ContactUs = () => {
       <div className="text-center mt-16 mb-10">
         <h1 className="text-3xl lg:text-5xl font-bold">Contact Us</h1>
         <p className="text-lg text-gray-500 mt-3">
-          We offer high-quality products at affordable prices.
+        From Seed to Success, We're Here to Help. Rooted in Relationships, Start the Conversation, Growing Together.
         </p>
       </div>
 
       <div className="flex flex-col md:flex-row items-center justify-center">
-      <div className="md:w-1/2">
-        <div className="text-lg font-bold mb-3">Head Office</div>
-        <p>Hathazari Road, Baluchara</p>
-        <p>Chittagong-4214, Bangladesh.</p>
-        <p>Tel: 0088-031-682739</p>
-        <p>E-mail: info@tazangroup.com</p>
-
-        <div className="text-lg font-bold mt-6">Liaison Office</div>
-        <p>382, Tangi Diverson Road (4th floor)</p>
-        <p>Mogbazar, Dhaka-1212</p>
-        <p>E-mail: tazanagro@gmail.com</p>
+      <div className="md:w-[40%] b">
+      
+          <Lottie animationData={animation} loop={true}></Lottie>
+        
       </div>
+
         <div className="md:w-1/2">
           <div className="text-lg font-bold mb-6">Get In Touch</div>
           <form action="#" className="w-full">
@@ -71,6 +67,20 @@ const ContactUs = () => {
             </div>
           </form>
         </div>
+      </div>
+
+      {/* Address part */}
+      <div className="md:w-1/2 ">
+        <div className="text-lg font-bold mb-3">Head Office</div>
+        <p>Hathazari Road, Baluchara</p>
+        <p>Chittagong-4214, Bangladesh.</p>
+        <p>Tel: 0088-031-682739</p>
+        <p>E-mail: info@tazangroup.com</p>
+
+        <div className="text-lg font-bold mt-6">Liaison Office</div>
+        <p>382, Tangi Diverson Road (4th floor)</p>
+        <p>Mogbazar, Dhaka-1212</p>
+        <p>E-mail: tazanagro@gmail.com</p>
       </div>
     </div>
   );
