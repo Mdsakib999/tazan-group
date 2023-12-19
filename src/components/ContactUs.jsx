263edbf3da59e963de8abe35190d209a7671f96d
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-19 16:38:08 +0600

    offic address and map added on footer section

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import animation from "../assets/Animation - 1702966470394.json";
 import Lottie from "lottie-react";
+import { FaLocationDot } from "react-icons/fa6";
 
 const ContactUs = () => {
   return (
@@ -8,19 +9,18 @@ const ContactUs = () => {
       <div className="text-center mt-16 mb-10">
         <h1 className="text-3xl lg:text-5xl font-bold">Contact Us</h1>
         <p className="text-lg text-gray-500 mt-3">
-        From Seed to Success, We're Here to Help. Rooted in Relationships, Start the Conversation, Growing Together.
+          From Seed to Success, We're Here to Help. Rooted in Relationships,
+          Start the Conversation, Growing Together.
         </p>
       </div>
 
       <div className="flex flex-col md:flex-row items-center justify-center">
-      <div className="md:w-[40%] b">
-      
+        <div className="md:w-[40%] ">
           <Lottie animationData={animation} loop={true}></Lottie>
-        
-      </div>
+        </div>
 
         <div className="md:w-1/2">
-          <div className="text-lg font-bold mb-6">Get In Touch</div>
+          <div className="text-2xl font-bold mb-4 ">Get In Touch</div>
           <form action="#" className="w-full">
             <div className="flex flex-wrap -mx-2 mb-6">
               <div className="w-full md:w-1/2 px-2 mb-4 md:mb-0">
@@ -60,9 +60,12 @@ const ContactUs = () => {
             <div className="flex items-center">
               <button
                 type="submit"
-                className="bg-blue-500 border-2 text-white px-6 py-3 rounded-full text-lg font-semibold  hover:bg-white hover:text-blue-500 border-blue-500"
+                className="group mt-2  border border-green-500 hover:border-none relative h-12 w-44 overflow-hidden rounded-lg font-bold bg-slate-50 text-lg shadow-md"
               >
-                Send Message
+                <div className="absolute inset-0 w-3 bg-gradient-to-r from-green-600 to-lime-300 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+                <span className="relative text-black group-hover:text-white">
+                  Send Message
+                </span>
               </button>
             </div>
           </form>
@@ -70,17 +73,23 @@ const ContactUs = () => {
       </div>
 
       {/* Address part */}
-      <div className="md:w-1/2 ">
-        <div className="text-lg font-bold mb-3">Head Office</div>
-        <p>Hathazari Road, Baluchara</p>
-        <p>Chittagong-4214, Bangladesh.</p>
-        <p>Tel: 0088-031-682739</p>
-        <p>E-mail: info@tazangroup.com</p>
+      <div className="flex flex-col md:flex-row items-center gap-7 lg:gap-0 justify-evenly my-10">
+        <div className="md:w-[35%] b">
+          <div className="text-3xl font-semibold mb-3">Head Office</div>
+          <FaLocationDot /> <p className="text-xl"> Hathazari Road, Baluchara</p>
+          <p className="text-xl">Chittagong-4214, Bangladesh.</p>
+          <p className="text-xl">Tel: 0088-031-682739</p>
+          <p className="text-xl">E-mail: info@tazangroup.com</p>
 
-        <div className="text-lg font-bold mt-6">Liaison Office</div>
-        <p>382, Tangi Diverson Road (4th floor)</p>
-        <p>Mogbazar, Dhaka-1212</p>
-        <p>E-mail: tazanagro@gmail.com</p>
+          <div className="text-3xl font-semibold mb-1 mt-6">Liaison Office</div>
+          <p className="text-xl">382, Tangi Diversion Road (4th floor)</p>
+          <p className="text-xl">Mogbazar, Dhaka-1212</p>
+          <p className="text-xl">E-mail: tazanagro@gmail.com</p>
+        </div>
+
+        <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10432.550414545298!2d91.81833422915041!3d22.41157129675362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd9137c580dbd%3A0xaeb6e95da2a45879!2sTazan%20Group!5e0!3m2!1sen!2sbd!4v1702977971780!5m2!1sen!2sbd" width="600" height="350" style={{border:0,}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -121,7 +121,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <Link to="/contact">
+        <Link to="#contact">
           <button
             className="px-3 lg:px-4 py-2 rounded-md
           text-black lg:text-xl font-bold bg-gradient-to-r from-green-600 to-lime-300 hover:from-lime-300 hover:to-green-600  ..."
