3bb03e72ae715baed521cd9f21894bf3e17997f8
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-20 11:51:35 +0600

    address icon done

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,6 +3,7 @@ import animation from "../assets/Animation - 1702966470394.json";
 import Lottie from "lottie-react";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
+import { BiSolidPhoneCall } from "react-icons/bi";
 
 
 const ContactUs = () => {
@@ -76,23 +77,22 @@ const ContactUs = () => {
 
       {/* Address part */}
       <div className="flex flex-col md:flex-row items-center gap-7 lg:gap-0 justify-evenly my-10">
-        <div className="md:w-[35%] ">
-          <div className="text-3xl font-semibold mb-3">Head Office</div>
-          <div className="flex items-center">
-          <FaLocationDot className="text-2xl"/> <p className="text-xl"> Hathazari Road, Baluchara</p>
-          </div>
-          <p className="text-xl">Chittagong-4214, Bangladesh.</p>
-          <p className="text-xl">Tel: 0088-031-682739</p>
-          <p className="text-xl">E-mail: info@tazangroup.com</p>
+        <div className="md:w-[38%] ">
+          <div className="text-2xl lg:text-3xl font-semibold mb-3">Head Office</div>
+          
+          <p className="text-xl"><FaLocationDot className="text-2xl inline-block"/> Hathazari Road, Baluchara.</p>
+          <p className=" text-xl ml-7 mb-2">Chittagong-4214, Bangladesh.</p>
+          <p className="text-xl "><BiSolidPhoneCall className="inline-block text-2xl"/> Tel: 0088-031-682739</p>
+          <p className="text-xl mt-2"><span><MdEmail className="inline-block text-2xl"/></span> E-mail: info@tazangroup.com</p>
 
-          <div className="text-3xl font-semibold mb-3 mt-6">Liaison Office</div>
+          <div className="text-2xl lg:text-3xl font-semibold mb-3 mt-6">Liaison Office</div>
           <p className="text-xl"><span><FaLocationDot className="inline-block text-2xl"></FaLocationDot></span> 382, Tangi Diversion Road (4th floor)</p>
           <p className="text-xl ml-7 mb-2">Mogbazar, Dhaka-1212</p>
           <p className="text-xl"><span><MdEmail className="inline-block text-2xl"/></span> E-mail: tazanagro@gmail.com</p>
         </div>
 
         <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10432.550414545298!2d91.81833422915041!3d22.41157129675362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd9137c580dbd%3A0xaeb6e95da2a45879!2sTazan%20Group!5e0!3m2!1sen!2sbd!4v1702977971780!5m2!1sen!2sbd" width="600" height="350" style={{border:0,}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe className="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10432.550414545298!2d91.81833422915041!3d22.41157129675362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd9137c580dbd%3A0xaeb6e95da2a45879!2sTazan%20Group!5e0!3m2!1sen!2sbd!4v1702977971780!5m2!1sen!2sbd" width="600" height="350" style={{border:0,}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
