525536a31ef45a0c4c7f5b32741be4ad5a417a14
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-26 15:36:11 +0600

    career

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 const Career = () => {
     return (
         <div>
-            <h1 className='mt-5 text-lg text-center font-semibold'>Where passion <span className='font-bold text-green-600'>Ignites</span>, Purpose guides 
+            <h1 className='mt-5 text-xl text-center font-semibold'>Where passion <span className='font-bold text-green-600'>Ignites</span>, Purpose guides 
             </h1>
             <h1 className='text-center font-semibold text-lg'>Discover your calling, Craft your professional journey.</h1>
 
             <img className='mt-5' src="https://i.ibb.co/dKhRdHm/concept-success-business-solid-business-team-standing-each-other-arms-crossed-front-him-160672-28525.png" alt="formal group picture" />
+
+            <div className='bg-black text-white px-4 py-6 shadow-lg'>
+                <p>Why Should Join Us?</p>
+                <p>Competitive Salary</p>
+                <p>Work-Life Balance</p>
+                <p>Paid Time Off</p>
+                <p>Provident Fund</p>
+            </div>
+
+
         </div>
     );
 };
