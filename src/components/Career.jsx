89acef460cbf17d0da63ec951d817503b2de709c
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-26 16:11:10 +0600

    career why should join done sm

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Career = () => {
-    return (
-        <div>
-            <h1 className='mt-5 text-xl text-center font-semibold'>Where passion <span className='font-bold text-green-600'>Ignites</span>, Purpose guides 
-            </h1>
-            <h1 className='text-center font-semibold text-lg'>Discover your calling, Craft your professional journey.</h1>
+  return (
+    <div>
+      <h1 className="mt-5 text-xl text-center font-semibold">
+        Where passion <span className="font-bold text-green-600">Ignites</span>,
+        Purpose guides
+      </h1>
+      <h1 className="text-center font-semibold text-lg">
+        Discover your calling, Craft your professional journey.
+      </h1>
 
-            <img className='mt-5' src="https://i.ibb.co/dKhRdHm/concept-success-business-solid-business-team-standing-each-other-arms-crossed-front-him-160672-28525.png" alt="formal group picture" />
+      <img
+        className="mt-5"
+        src="https://i.ibb.co/dKhRdHm/concept-success-business-solid-business-team-standing-each-other-arms-crossed-front-him-160672-28525.png"
+        alt="formal group picture"
+      />
 
-            <div className='bg-black text-white px-4 py-6 shadow-lg'>
-                <p>Why Should Join Us?</p>
-                <p>Competitive Salary</p>
-                <p>Work-Life Balance</p>
-                <p>Paid Time Off</p>
-                <p>Provident Fund</p>
-            </div>
-
-
+      <div className="bg-black text-white px-4 py-6 shadow-lg">
+        <p className="font-bold text-xl text-center mb-4">
+          Why Should Join Us?
+        </p>
+        <div className="flex justify-evenly text-center font-semibold">
+          <p className="border-r-2">Competitive Salary</p>
+          <p className="border-r-2">Work-Life Balance</p>
+          <p className="border-r-2">Paid Time Off</p>
+          <p >Provident Fund</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Career;
