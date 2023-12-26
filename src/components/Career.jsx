d73ecb1baee77c976cda782cdf4440e6dcffd1f7
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-26 14:31:07 +0600

    career page

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Career = () => {
     return (
         <div>
-            <h1>I'm career page</h1>
+            <h1 className='mt-5 text-lg text-center font-semibold'>Where passion <span className='font-bold text-green-600'>Ignites</span>, Purpose guides 
+            </h1>
+            <h1 className='text-center font-semibold text-lg'>Discover your calling, Craft your professional journey.</h1>
         </div>
     );
 };
