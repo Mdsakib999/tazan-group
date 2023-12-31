5e152bf285338c563634850fae8dfdd62b0107af
Mdsakib999 <sakibabrar999@gmail.com>
2023-12-31 12:17:55 +0600

    career job lg

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -29,11 +29,12 @@ const Career = () => {
         </div>
       </div>
 
-      <h1 className=" text-center mt-2 text-xl font-bold underline">Current Opening</h1>
+      <h1 className="text-center mt-2 lg:mt-5 text-xl lg:text-4xl font-bold underline">Current Opening</h1>
 
-      <div className="w-[95%] mt-3 px-2 py-2 mx-auto flex justify-between items-center bg-gray-100 rounded-lg mb-8 shadow-lg">
-        <p className="font-semibold text-base">Marketing Officer</p>
-        <p className="font-semibold text-base">Last Date 30 Jan</p>
+      <div className="w-[95%] lg:w-[85%] mt-3 lg:mt-7 px-2 py-2 lg:px-4 lg:py-3 mx-auto flex justify-between items-center bg-gray-100 rounded-lg mb-8 shadow-lg">
+        <p className="font-semibold text-base lg:text-xl">Marketing Officer</p>
+        <p className="hidden lg:block font-semibold text-base lg:text-xl">Mid Senior</p>
+        <p className="font-semibold text-base lg:text-xl">Last Date 30 Jan</p>
         <p className="btn btn-warning">View Details</p>
       </div>
 
