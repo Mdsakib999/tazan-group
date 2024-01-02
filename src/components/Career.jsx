bac581f1d8ead27482ab218658955d1c708ebb6b
Mdsakib999 <sakibabrar999@gmail.com>
2024-01-02 12:19:32 +0600

    career responsive done

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -44,29 +44,29 @@ const Career = () => {
 
       {/*  */}
       <div className=" my-20 grid grid-cols-2 lg:grid-cols-3 gap-0">
-      <div className=" flex flex-col items-center justify-center py-12 hover:bg-slate-100 border-r-2 border-b-2">
-        <FaSellsy className="text-6xl" />
-        <p className="text-2xl font-semibold">Marketing and Sales</p>
+      <div className=" flex flex-col items-center justify-center py-5 lg:py-12 hover:bg-slate-100 border-r-2 border-b-2">
+        <FaSellsy className="text-5xl lg:text-6xl" />
+        <p className="text-xl lg:text-2xl font-semibold">Marketing and Sales</p>
       </div>
-      <div className="flex flex-col items-center justify-center py-12 lg:border-r-2 border-b-2 hover:bg-slate-100">
-        <FaLaptopCode className="text-6xl" />
-        <p className="text-2xl font-semibold">Software & IT</p>
+      <div className="flex flex-col items-center justify-center py-5 lg:py-12 lg:border-r-2 border-b-2 hover:bg-slate-100">
+        <FaLaptopCode className="text-5xl lg:text-6xl" />
+        <p className="text-xl lg:text-2xl font-semibold">Software & IT</p>
       </div>
-      <div className="flex flex-col items-center justify-center py-12 border-b-2 border-r-2 lg:border-r-0 hover:bg-slate-100">
-        <FaShippingFast className="text-6xl" />
-        <p className="text-2xl font-semibold">Logistic & Legal</p>
+      <div className="flex flex-col items-center justify-center py-5 lg:py-12 border-b-2 border-r-2 lg:border-r-0 hover:bg-slate-100">
+        <FaShippingFast className="text-5xl lg:text-6xl" />
+        <p className="text-xl lg:text-2xl font-semibold">Logistic & Legal</p>
       </div>
-      <div className="flex flex-col items-center justify-center  py-12 lg:border-r-2 border-b-2 lg:border-b-0 hover:bg-slate-100">
-        <RiCustomerService2Fill className="text-6xl" />
-        <p className="text-2xl font-semibold">Customer Service</p>
+      <div className="flex flex-col items-center justify-center  py-5 lg:py-12 lg:border-r-2 border-b-2 lg:border-b-0 hover:bg-slate-100">
+        <RiCustomerService2Fill className="text-5xl lg:text-6xl" />
+        <p className="text-xl lg:text-2xl font-semibold">Customer Service</p>
       </div>
-      <div className="flex flex-col items-center justify-center  py-12 border-r-2 hover:bg-slate-100">
-        <RiMoneyDollarCircleLine className="text-6xl" />
-        <p className="text-2xl font-semibold">Accounts & Finance</p>
+      <div className="flex flex-col items-center justify-center  py-5 lg:py-12 border-r-2 hover:bg-slate-100">
+        <RiMoneyDollarCircleLine className="text-5xl lg:text-6xl" />
+        <p className="text-xl lg:text-2xl font-semibold">Accounts & Finance</p>
       </div>
-      <div className=" flex flex-col items-center justify-center  py-12 hover:bg-slate-100">
-        <SiWebmoney className="text-6xl" />
-        <p className="text-2xl font-semibold">Digital Marketing</p>
+      <div className=" flex flex-col items-center justify-center  py-5 lg:py-12 hover:bg-slate-100">
+        <SiWebmoney className="text-5xl lg:text-6xl" />
+        <p className="text-xl lg:text-2xl font-semibold">Digital Marketing</p>
       </div>
     </div>
       
